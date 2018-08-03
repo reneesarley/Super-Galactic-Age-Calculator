9daa36e88348269284545aec0a329562dc337ab1
Renee Sarley <reneesarley@gmail.com>
2018-08-03 15:41:45 -0700

    refactor getUsersAgeInYears to calculate for all planets

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -5,40 +5,44 @@ describe ('Age', function(){
   let testBDay = new Date(1982, 4, 21, 4, 30, 0, 0)
   ///setup variables there
   beforeEach(function(){
-    newAge = new Age();
-    //(year, month, day, hours, minutes, seconds, milliseconds
-    newAge.earthAgeYears = 35.5
-    newAge.earthLifeExpectency = 88
-    newAge.userBday = testBDay;
+    newAge = new Age(testBDay);
+    newAge.getUsersAgeInYears();
+    // newAge.earthAgeYears = 35.5
+    // newAge.earthLifeExpectency = 88
+    // newAge.userBday = testBDay;
     //establish test instances here
   });
 //add tests here
-  it('should convert earth years to seconds', function(){
-    expect (newAge.convertEarthYearsToSeconds(this.earthAgeYears)).toEqual(1120294800);
-  })
+  // it('should convert earth years to seconds', function(){
+  //   expect (newAge.convertEarthYearsToSeconds(this.earthAgeYears)).toEqual(1120294800);
+  // })
 
-  it('this will convert earth years to planet years', function(){
-    newAge.getPlanetAges();
-    expect(newAge.planetAges.mercury).toEqual(8.52);
-    expect(newAge.planetAges.venus).toEqual(22.01);
-    expect(newAge.planetAges.mars).toEqual(66.74);
-    expect(newAge.planetAges.jupiter).toEqual(421.03);
-  })
+  // it('this will convert earth years to planet years', function(){
+  //   newAge.getPlanetAges();
+  //   expect(newAge.planetAges.mercury).toEqual(8.52);
+  //   expect(newAge.planetAges.venus).toEqual(22.01);
+  //   expect(newAge.planetAges.mars).toEqual(66.74);
+  //   expect(newAge.planetAges.jupiter).toEqual(421.03);
+  // })
 
-  it('should calculate years left to live for all planets', function(){
-    newAge.getPlanetAges();
-    newAge.getYearsLeft();
-    expect(newAge.yearsLeft.mercury).toEqual((88*.24)-8.52);
-    expect(newAge.yearsLeft.venus).toEqual((88*.62)-22.01);
-    expect(newAge.yearsLeft.mars).toEqual((88*1.88)-66.74);
-    expect(newAge.yearsLeft.jupiter).toEqual((88*11.86)-421.03);
-  })
+  // it('should calculate years left to live for all planets', function(){
+  //   newAge.getPlanetAges();
+  //   newAge.getYearsLeft();
+  //   expect(newAge.yearsLeft.mercury).toEqual((88*.24)-8.52);
+  //   expect(newAge.yearsLeft.venus).toEqual((88*.62)-22.01);
+  //   expect(newAge.yearsLeft.mars).toEqual((88*1.88)-66.74);
+  //   expect(newAge.yearsLeft.jupiter).toEqual((88*11.86)-421.03);
+  // })
 
   it('should return the users age in earth years', function(){
     newAge.getUsersAgeInYears();
     let currentDateTime = new Date();
     let age = (currentDateTime - testBDay)/1000 /60 /60 /24 /365.25
-    let ageRounded = Math.round(age*100)/100;
-    expect(newAge.planetAges.earth).toEqual(ageRounded);
+    // let ageRounded = Math.round(age*100)/100;
+    expect(newAge.planetAges.earth).toEqual(Math.round(age*100)/100);
+    expect(newAge.planetAges.mercury).toEqual(Math.round((age * .24)*100)/100);
+    expect(newAge.planetAges.venus).toEqual(Math.round((age * .62)*100)/100);
+    expect(newAge.planetAges.mars).toEqual(Math.round((age * 1.88)*100)/100);
+    expect(newAge.planetAges.jupiter).toEqual(Math.round((age * 11.86)*100)/100);
   })
 })
--- src/age.js
+++ src/age.js
@@ -1,45 +1,45 @@
 export class Age{
-  constructor(userBday){
-    this.usersBday;
-    this.currentTime;
-    this.earthAgeYears;
-    this.earthLifeExpectency;
+  constructor(birthday){
+    this.usersBday = birthday;
+    // this.currentTime;
+    // this.planetAges.earth;
+    // this.earthLifeExpectency;
     this.planetAges = {};
-    this.planetLifeExpectancies={};
-    this.yearsLeft={};
+    // this.planetLifeExpectancies={};
+    // this.yearsLeft={};
     this.allPlanets=['earth', 'mercury', 'venus', 'mars','jupiter']
     this.earthVersusPlanetRate ={earth: 1,
                                  mercury:.24,
                                  venus: .62,
                                  mars: 1.88,
                                  jupiter: 11.86};
-    // this.getPlanetAges();
+    // this.getUsersAgeInYears();
   }
 
-  convertEarthYearsToSeconds(){
-   let usersAgeInSeconds = this.earthAgeYears * 365.25 * 24 * 60 * 60;
-   return usersAgeInSeconds;
-  }
-  getPlanetAges(){
-    for (let i =0 ; i < this.allPlanets.length; i++){
-      let planet = this.allPlanets[i];
-    this.planetAges[planet] = this.earthAgeYears * this.earthVersusPlanetRate[planet];
-    }
-  }
-  getYearsLeft(){
-    for (let i =0 ; i < this.allPlanets.length; i++){
-      let planet = this.allPlanets[i];
-    this.yearsLeft[planet] = (this.earthLifeExpectency * this.earthVersusPlanetRate[planet]) - this.planetAges[planet];
-    }
-  }
+  // convertEarthYearsToSeconds(){
+  //  let usersAgeInSeconds = this.planetAges.earth * 365.25 * 24 * 60 * 60;
+  //  return usersAgeInSeconds;
+  // }
+  // getPlanetAges(){
+  //   for (let i =0 ; i < this.allPlanets.length; i++){
+  //     let planet = this.allPlanets[i];
+  //   this.planetAges[planet] = this.planetAges.earth * this.earthVersusPlanetRate[planet];
+  //   }
+  // }
+  // getYearsLeft(){
+  //   for (let i =0 ; i < this.allPlanets.length; i++){
+  //     let planet = this.allPlanets[i];
+  //   this.yearsLeft[planet] = (this.earthLifeExpectency * this.earthVersusPlanetRate[planet]) - this.planetAges[planet];
+  //   }
+  // }
   getUsersAgeInYears(){
     let currentDateTime = new Date();
-    let planet = 'earth'
-    console.log(this.userBday);
-    console.log(currentDateTime);
-    let ageInYears = currentDateTime - this.userBday;
-    ageInYears = ageInYears /1000 /60 /60 /24 /365.25;
-    this.planetAges[planet] = Math.round(ageInYears * 100) / 100;
-    console.log(this.planetAges[planet]);
+    let earthAgeInYears = (currentDateTime - this.usersBday) /1000 /60 /60 /24 /365.25;
+    for (let i =0 ; i < this.allPlanets.length; i++){
+      // let planet = 'earth';
+      let planet = this.allPlanets[i];
+      this.planetAges[planet] = Math.round((earthAgeInYears * this.earthVersusPlanetRate[planet] )* 100) / 100;
+      console.log(this.planetAges[planet]);
+    }
   }
 }
