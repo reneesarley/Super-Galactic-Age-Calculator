7180b224b718b914c3424cc0cec424ac87996b5c
Renee Sarley <reneesarley@gmail.com>
2018-08-03 12:55:14 -0700

    add getPlanetAges method and test for mercury

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,17 +1,21 @@
 import{Age} from '../src/age';
 
 describe ('Age', function(){
-
+  let newAge;
   ///setup variables there
   beforeEach(function(){
-
+    newAge = new Age();
+    newAge.earthAgeYears = 35.5
     //establish test instances here
   });
 //add tests here
   it('should convert earth years to seconds', function(){
-    let newAge = new Age();
-    newAge.usersAge = 35.5
-    expect (newAge.convertEarthYearsToSeconds(this.usersAge)).toEqual(1120294800);
+    expect (newAge.convertEarthYearsToSeconds(this.earthAgeYears)).toEqual(1120294800);
+  })
+
+  it('this will convert earth years to earth planet years', function(){
+    newAge.getPlanetAges();
+    expect(newAge.planetAges.mercury).toEqual(8.52);
   })
 
 })
--- src/age.js
+++ src/age.js
@@ -1,15 +1,21 @@
 export class Age{
   constructor(){
     this.currentTime;
-    this.usersAge;
+    this.earthAgeYears;
     this.usersBday;
-    this.planetAges={};
+    this.planetAges = {};
     this.planetlifeExpectancies={};
+    // this.getPlanetAges();
   }
 
   convertEarthYearsToSeconds(){
-   let usersAgeInSeconds = this.usersAge * 365.25 * 24 * 60 * 60;
+   let usersAgeInSeconds = this.earthAgeYears * 365.25 * 24 * 60 * 60;
    return usersAgeInSeconds;
+  }
+  getPlanetAges(){
+    console.log(this.planetAges.mercury);
+    this.planetAges.mercury = this.earthAgeYears * .24
+    console.log(this.planetAges.mercury);
   }
 
 }
