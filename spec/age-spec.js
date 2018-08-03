6578fd000cd79dcd7cdd5f530fb92a61feed273d
Renee Sarley <reneesarley@gmail.com>
2018-08-03 10:42:27 -0700

    add age-spec file with initial setup

---
+++ spec/age-spec.js
@@ -0,0 +1,15 @@
+import{Age} from '../src/age';
+
+describe ('Age', function(){
+
+  ///setup variables there
+  beforeEach(function(){
+    //establish test instances here
+  });
+//add tests here
+  it('should convert earth years to seconds'function(){
+  // expect ().toEqual();
+
+  })
+
+})
